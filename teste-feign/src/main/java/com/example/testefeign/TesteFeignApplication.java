3cb3fb4c473cb6f029d600cd282674d78d01b89c
vitor.rodrigues <vitor.rodrigues>
2023-01-29 04:56:48 +0000

    teste

--- teste-feign/src/main/java/com/example/testefeign/TesteFeignApplication.java
+++ teste-feign/src/main/java/com/example/testefeign/TesteFeignApplication.java
@@ -1,21 +1,17 @@
 package com.example.testefeign;
 
 
-import com.example.testefeign.client.GreetingClient;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TesteFeignApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TesteFeignApplication.class, args);
     }
 
